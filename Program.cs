cbfa400661a8c77573850e4a2ff920f8a0db46af
Brian <briancmuhammad@yahoo.com>
2024-09-29 15:03:11 -0700

    Added update, created, and delete endpoints

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,18 +26,49 @@ app.UseCors(p => p.WithOrigins("http://localhost:3000")
 
 app.UseHttpsRedirection();
 
+//Endpoint for getting all houses
 app.MapGet("/houses", (IHouseRepository repo) => repo.GetAll())
 .Produces<HouseDto[]>(StatusCodes.Status200OK);
+
+//Endpoint for getting details for one house
 app.MapGet("/house/{houseId:int}", async (int houseId, IHouseRepository repo) =>
 {
     var house = await repo.Get(houseId);
     if (house == null)
         return Results.Problem($"House with Id {houseId} not found.",
         statusCode: 404);
-        return Results.Ok(house);
+    return Results.Ok(house);
 }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
 
+//Endpoint for creating houses
+app.MapPost("/houses", async ([FromBody] HouseDetailDto dto,
+IHouseRepository repo) =>
+{
+    var newHouse = repo.Add(dto);
+    return Results.Created($"/house/{newHouse.Id}", newHouse);
+}).Produces<HouseDetailDto>(StatusCodes.Status201Created);
 
+//Endpoint for updating houses
+app.MapPut("/houses", async ([FromBody] HouseDetailDto dto,
+IHouseRepository repo) =>
+{
+    if (await repo.Get(dto.Id) == null)
+        return Results.Problem($"House {dto.Id} not found",
+        statusCode: 404);
+    var updatedHouse = await repo.Update(dto);
+    return Results.Ok(updatedHouse);
+}).Produces<HouseDetailDto>(StatusCodes.Status200OK);
+
+//Endpoint for deleting houses
+app.MapDelete("/houses/{houseId:int}", async (int houseId,
+IHouseRepository repo) =>
+{
+    if (await repo.Get(houseId) == null)
+        return Results.Problem($"house {houseId} not found",
+        statusCode: 404);
+    await repo.Delete(houseId);
+    return Results.Ok();
+}).ProducesProblem(404).Produces(StatusCodes.Status200OK);
 
 app.Run();
 
